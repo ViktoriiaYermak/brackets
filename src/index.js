a2f0361712e3205552784f2f8fe38cfded44430c
ViktoriiaYermak <yermakvictoriia@gmail.com>
2023-10-26 17:53:10 +0300

    feat: finished the task solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const string = [];
+  const openBrackets = [];
+  const closeBrackets = [];
+
+  for (const [open, close] of bracketsConfig) {
+    openBrackets.push(open);
+    closeBrackets.push(close);
+  }
+
+  for (const char of str) {
+    if (openBrackets.includes(char) && closeBrackets.includes(char)) {
+      if (string.length > 0 && string[string.length - 1] === char) {
+        string.pop();
+      } else {
+        string.push(char);
+      }
+    } else if (openBrackets.includes(char)) {
+      string.push(char);
+    } else if (closeBrackets.includes(char)) {
+      const lastOpen = string.pop();
+      const expectedOpen = openBrackets[closeBrackets.indexOf(char)];
+      if (lastOpen !== expectedOpen) {
+        return false;
+      }
+    }
+  }
+  return string.length === 0;
+};
